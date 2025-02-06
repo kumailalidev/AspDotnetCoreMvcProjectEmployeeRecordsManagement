61ba9dc425c4460a6c50a6975538da59e8f004b9
kumailalidev <kumailalidev@gmail.com>
2025-02-06 21:36:22 +0500

    Updated EmployeeViewModel

--- EFCoreWithAspDotnetCore/ViewModels/EmployeeViewModel.cs
+++ EFCoreWithAspDotnetCore/ViewModels/EmployeeViewModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 using EFCoreWithAspDotnetCore.Models;
 
@@ -7,6 +8,24 @@ namespace EFCoreWithAspDotnetCore.ViewModels
     // Contains properties specific to Employee specific views.
     public class EmployeeViewModel
     {
-        public Employee Employee { get; set; }
+        public EmployeeViewModel()
+        {
+        }
+        public int EmployeeId { get; set; } // EF Core assumes Id or {ClassName}Id as PK
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public bool IsActive { get; set; }
+
+        //Relationship with Department
+
+        [ForeignKey("Department")]
+        public int DepartmentId { get; set; } //Foreign Key
+        public Department? Department { get; set; } //Reference navigation property
+
     }
 }
