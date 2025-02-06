e7b3fe44f897bf7588c7fedfb1c4fb48d302cf3d
kumailalidev <kumailalidev@gmail.com>
2025-02-07 02:21:10 +0500

    Implemented Sorting

--- EFCoreWithAspDotnetCore/Controllers/EmployeeController.cs
+++ EFCoreWithAspDotnetCore/Controllers/EmployeeController.cs
@@ -19,16 +19,51 @@ namespace EFCoreWithAspDotnetCore.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Index(string searchString)
+        public async Task<IActionResult> Index(string searchString, string sortOrder)
         {
             // Get all the employees from database
             List<EmployeeViewModel> employees = await _employeeRepository.GetAllAsync();
 
             // Filter employees by name
-            string queryString = searchString.ToLower();
-            if (!String.IsNullOrEmpty(queryString))
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                string queryString = searchString.ToLower();
                 employees = employees.Where(employee => employee.FirstName.ToLower().Contains(queryString) ||
                     employee.LastName.ToLower().Contains(queryString)).ToList();
+
+            }
+
+            ViewData["NameSortParam"] = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["DateOfBirthSortParam"] = sortOrder == "date_asc" ? "date_desc" : "date_asc";
+            ViewData["IsActiveSortParam"] = sortOrder == "isactive_asc" ? "isactive_desc" : "isactive_asc";
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    // Sort employees by FirstName in descending order
+                    employees = employees.OrderByDescending(employee => employee.FirstName).ToList();
+                    break;
+                case "date_asc":
+                    // Sort employees by DOB in ascending order
+                    employees = employees.OrderBy(employee => employee.DateOfBirth).ToList();
+                    break;
+                case "date_desc":
+                    // Sort employees by DOB in descending order
+                    employees = employees.OrderByDescending(employee => employee.DateOfBirth).ToList();
+                    break;
+                case "isactive_desc":
+                    // Sort employees by IsActive in descending order
+                    employees = employees.OrderByDescending(employee => employee.IsActive).ToList();
+                    break;
+                case "isactive_asc":
+                    // Sort employees by IsActive ascending order
+                    employees = employees.OrderBy(employee => employee.IsActive).ToList();
+                    break;
+                default:
+                    // Sort employees by FirstName in ascending order
+                    employees = employees.OrderBy(employee => employee.FirstName).ToList();
+                    break;
+            }
 
             return View(employees);
         }
